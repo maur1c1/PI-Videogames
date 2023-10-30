62a8f1d9d08483122007925b2d908ed7fe31e504
maur1c1 <{ID}+{username}@users.noreply.github.com>
2023-10-30 17:00:28 -0600

    implemented-endopointrouter

---
+++ api/src/routes/genres.js
@@ -0,0 +1,29 @@
+const { Router } = require("express");
+
+const genreRouter = Router();
+
+//Implementamos mis endpoints
+
+
+genreRouter.get("/",function (req, res){
+
+});
+
+genreRouter.get("/:name", function (req, res){
+
+});
+
+genreRouter.put("/", function (req, res){
+
+});
+
+genreRouter.post("/", function (req, res) {
+
+})
+
+genreRouter.delete("/",function (req, res){
+
+});
+
+module.exports = genreRouter;
+
